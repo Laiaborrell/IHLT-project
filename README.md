2c075cbb15d24adfc2a8977b604b4841ae5dba61
unknown <laiaborrell27@gmail.com>
2021-11-24 19:59:18 +0100

    update.

--- README.md
+++ README.md
@@ -2,10 +2,16 @@
 
 ## Què utilitzen els 4 primers classificats del concurs:
 
-1) Dictionaries, Distributional thesaurus, Monolingual corpora, Wikipedia, WordNet, Distributional similarity, KBSimilarity, Lemmatizer, POS tagger, SMT, String similarity, Textual entailment i posa other també.
+1) Dictionaries, Distributional thesaurus, Monolingual corpora, Wikipedia, WordNet, 
+Distributional similarity, KBSimilarity, Lemmatizer, POS tagger, SMT, String similarity, Textual entailment i 
+posa other també.
 
-2) Aquest té dos runs, en el que pilla menys cosa agafa: Monolingual corpora, WordNet, distributional similarity, KB similarity, Lemmatizer, Lexical substitution, Named Entity recognition, POS tagger i Syntax.
+2) Aquest té dos runs, en el que pilla menys cosa agafa: Monolingual corpora, WordNet, 
+distributional similarity, KB similarity, Lemmatizer, Lexical substitution, Named Entity 
+recognition, POS tagger i Syntax.
 
-3) Wikipedia, WordNet, Alignment, Distributional similarity, KB Similarity, Lemmatizer, Machine Learning, Word Sense Disambiguation i posa other també.
+3) Wikipedia, WordNet, Alignment, Distributional similarity, KB Similarity, Lemmatizer, Machine Learning, 
+Word Sense Disambiguation i posa other també.
 
-4) Monolingual corpora, WordNet, KB Similarity, Lemmatizer, Lexical Substitution, MT evaluation i String similarity. També posa other.
+4) Monolingual corpora, WordNet, KB Similarity, Lemmatizer, Lexical Substitution, MT evaluation i String similarity. 
+També posa other.
--- project.py
+++ project.py
@@ -1,18 +1,41 @@
 import pandas as pd
 
 def read_data():
-    #files = ['MSRpar', 'MSRvid', 'SMTeuroparl', 'surprise.OnWN', 'surprise.SMTnews']
-    files = ['MSRvid', 'SMTeuroparl', 'surprise.OnWN', 'surprise.SMTnews']
-    dt = pd.read_csv(f'test-gold/STS.input.{files[0]}.txt',sep='\t',header=None)
-    for file in files:
-        if file !=files[0]:
-            new_dt = pd.read_csv(f'test-gold/STS.input.{file}.txt',sep='\t',header=None)
+    #test_files = ['MSRpar', 'MSRvid', 'SMTeuroparl', 'surprise.OnWN', 'surprise.SMTnews']
+    #train_files = ['MSRpar', 'MSRvid', 'SMTeuroparl']
+    train_files = ['MSRvid','SMTeuroparl']
+    test_files = ['MSRvid','SMTeuroparl','surprise.OnWN','surprise.SMTnews']
+    dt_train = pd.read_csv(f'train/STS.input.{train_files[0]}.txt',sep='\t',header=None)
+    gs_train = pd.read_csv(f'train/STS.gs.{train_files[0]}.txt',sep='\t',header=None)
+    dt_test = pd.read_csv(f'test-gold/STS.input.{test_files[0]}.txt', sep='\t', header=None)
+    gs_test = pd.read_csv(f'test-gold/STS.gs.{test_files[0]}.txt', sep='\t', header=None)
+    for train_file,test_file in zip(train_files,test_files):
+        if test_file !=test_files[0]: #si no es la primera iteracio
+            new_dt_train = pd.read_csv(f'train/STS.input.{train_file}.txt', sep='\t', header=None)
+            new_gs_train = pd.read_csv(f'train/STS.gs.{train_file}.txt', sep='\t', header=None)
+            new_dt_test = pd.read_csv(f'test-gold/STS.input.{test_file}.txt',sep='\t',header=None)
+            new_gs_test = pd.read_csv(f'test-gold/STS.gs.{test_file}.txt', sep='\t', header=None)
+
             #concatenate all the files in a single dataframe
-            dt = pd.concat([dt,new_dt],axis=0)
-    print(dt.shape)
-    print(dt.head())
+            dt_train = pd.concat([dt_train,new_dt_train],axis=0)
+            gs_train =  pd.concat([gs_train,new_gs_train],axis=0)
+            dt_test = pd.concat([dt_test, new_dt_test], axis=0)
+            gs_test = pd.concat([gs_test, new_gs_test], axis=0)
+
+    dt_train = pd.concat([dt_train,gs_train],axis=1)
+    dt_train.columns = [0,1,'gs']
+    dt_test = pd.concat([dt_test,gs_test],axis=1)
+    dt_test.columns = [0, 1, 'gs']
+
+    print(dt_train.head())
+    print(dt_test.head())
+    return dt_train,dt_test
 
 
 
 if __name__ == '__main__':
-    read_data()
+    #READING THE DATA
+    dt_train,dt_test = read_data()
+
+    #PREPROCESSING DATA
+
